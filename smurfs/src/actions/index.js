cb1bdd1f34682f750fb3cdb8a71857ca62e89264
Rachell Sincere <rachellsincere@gmail.com>
2019-08-05 18:34:09 -0400

    added the actions and action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +16,25 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING });
+  axios
+      .get('http://localhost:3333/smurfs')
+      .then(response =>
+          dispatch({ type: SUCCESS, payload: response.data.results}))
+          
+      .catch(error => dispatch({ type: FAILURE, payload: error}));
+};
+
+export const addSmurf = smurf => dispatch => {
+  dispatch({ type: ADD_SMURF });
+  axios
+    .post(url, smurf)
+    .then(response => {
+      dispatch({ type: SUCCESS, payload: response.data });
+    })
+    .catch(error => {
+      dispatch({type: FAILURE, payload: error});
+    })
+
+  }
